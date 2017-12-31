c1026b5eea198b509b0af5e97292df6e621931ae
David Delgado <djd9062@gmail.com>
2017-12-31 15:44:15 -0600

    removed percent

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const director = require('director');
-const Server   = require('./lib/server-2');
+const Server = require('./lib/server-2');
 
 // Create a router for GET and POST requests to the app
 const router = new director.http.Router({
-    '/': {
-        post: Server.postResponse,
-        get: Server.getResponse
-    }
+	'/': {
+		post: Server.postResponse,
+		get: Server.getResponse
+	}
 });
 
 // Check if the `--dev` flag was passed
